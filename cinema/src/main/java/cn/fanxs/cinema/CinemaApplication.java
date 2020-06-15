f508c24c71233169a6ca629b5a1079ce52869c85
18238595143 <fanxing@fanxs.cn>
2020-06-15 20:55:23 +0800

    初始化创建

---
+++ cinema/src/main/java/cn/fanxs/cinema/CinemaApplication.java
@@ -0,0 +1,13 @@
+package cn.fanxs.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApplication.class, args);
+    }
+
+}
